d5ff7355690cd3477ed23716967031b4d8bd1774
Claudio Terogene <cterogene@gmail.com>
2023-11-22 12:39:36 -0500

    added the assets and the floating island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,28 +2,46 @@ import React from 'react'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
+import { Island } from '../models/Island'
 const Home = () => {
-  return (
-    <section className='w-full h-screen relative'>
-       {/*<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1,4.7,0];
+    if(window.innerWidth < 768){
+      screenScale = [0.9,0.9,0.9];
+
+    } else{
+      screenScale = [1,1,1];
+    }
+    return [screenScale, screenPosition, rotation]
+    
+    }
+const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+return (
+  <section className='w-full h-screen relative'>
+    {/*<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
   </div> */}
 
-      <Canvas className='w-full h-screen transparent' 
-      camera={{near: 0.1, for: 1000 }} >
-         <Suspense fallback={<Loader />} > 
-         <directionalLight />
-         <ambientLight />
-         <pointLight />
-         <spotLight />
-         <hemisphereLight />
+    <Canvas className='w-full h-screen transparent'
+      camera={{ near: 0.1, for: 1000 }} >
+      <Suspense fallback={<Loader />} >
+        <directionalLight />
+        <ambientLight />
+        <pointLight />
+        <spotLight />
+        <hemisphereLight />
+        <Island  
+        position ={islandPosition} scale={islandScale} rotation={islandRotation}/>
 
-         </Suspense>
+      </Suspense>
 
-      </Canvas>
+    </Canvas>
 
-    </section>
-  )
+  </section>
+)
 }
 
 export default Home
