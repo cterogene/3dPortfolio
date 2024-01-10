d94f505435a4dd1846f9a6ca6eedd9c72783f241
Claudio Terogene <cterogene@gmail.com>
2024-01-10 10:19:40 -0500

    Added the primitive sky and the sun view to the island.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import { Island } from '../models/Island'
+import Sky from '../models/Sky'
+
 const Home = () => {
 
   const adjustIslandForScreenSize = () => {
@@ -28,11 +30,12 @@ return (
     <Canvas className='w-full h-screen transparent'
       camera={{ near: 0.1, for: 1000 }} >
       <Suspense fallback={<Loader />} >
-        <directionalLight />
-        <ambientLight />
-        <pointLight />
-        <spotLight />
-        <hemisphereLight />
+        <directionalLight position={[1,1,1]} intensity={2}/>
+        <ambientLight intensity={0.5}/>
+        <hemisphereLight  skyColor="
+        #b1e1ff" groundColor="#000000" intensity={1}/>
+        <Sky />
+       
         <Island  
         position ={islandPosition} scale={islandScale} rotation={islandRotation}/>
 
