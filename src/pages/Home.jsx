5f6dcd1212820349e96eaaf10843aeee51c339eb
Claudio Terogene <cterogene@gmail.com>
2024-01-14 15:15:50 -0500

    Sky movement added and now moving with the island.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,7 +55,7 @@ return (
         <ambientLight intensity={0.5}/>
         <hemisphereLight  skyColor="
         #b1e1ff" groundColor="#000000" intensity={1}/>
-        <Sky />
+        <Sky isRotating={isRotating}/>
        <Bird />
         <Island  
         position ={islandPosition} scale={islandScale} rotation={islandRotation} 
