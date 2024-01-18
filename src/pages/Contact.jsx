18393d85bed0a247f9f773f0c11694ab374c04f8
Claudio Terogene <cterogene@gmail.com>
2024-01-18 06:55:48 -0500

    added email js public key and service id in a env file.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,9 +12,25 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    emailjs.sendForm(
-      
-    )
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: 'Claudio',
+        from_email: form.email,
+        to_email:'cterogene@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+
+    ).then(() => {
+      setIsLoading(false);
+      setForm({name: '', email: '', message: ''});
+    }).catch((error) => {
+      setIsLoading(false);
+      console.log(error);
+    })
   };
 
   const handleFocus = () => { };
@@ -32,7 +48,7 @@ const Contact = () => {
               type="text"
               name="name"
               className='input'
-              placeholder='John'
+              placeholder='John doe'
               required
               value={form.name}
               onChange={handleChange}
