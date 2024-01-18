26a292c67c2d91047bf3f96020fd23ff8e4b7c46
Claudio Terogene <cterogene@gmail.com>
2024-01-17 20:02:51 -0500

    Email form added and installed the Email js package.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: '' })
-  const handleChange = () => { };
+  const [isLoading, setIsLoading] = useState(false);
+  const handleChange = (e) => { 
+    setForm({...form, [e.target.name]: e.target.value })
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.sendForm(
+      
+    )
+  };
+
   const handleFocus = () => { };
   const handleBlur = () => { };
+  
 
   return (
-    <section className='relative flex lg:flex-row flex-col'>
+    <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text mt-14'> Get in Touch</h1>
         <form className='w-full flex flex-col gap-7 mt-14'>
@@ -59,9 +74,10 @@ const Contact = () => {
           <button 
             type='submit'
             className='btn'
+              disabled={isLoading}
               onFocus={handleFocus}
               onBlur={handleBlur} >
-                Submit
+                {isLoading ? 'Sending...' : 'Send Message'}
 
           </button>
         </form>
