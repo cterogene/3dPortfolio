334b78269bbb51af409790353996d61f71db54bd
Claudio Terogene <cterogene@gmail.com>
2024-01-25 14:38:50 -0500

    Fox appears normally in the screen with proper light and positions

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,9 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, Suspense } from 'react'
 import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber';
+
+import Fox from '../models/Fox'
+import Loader from '../components/Loader';
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -101,6 +105,25 @@ const Contact = () => {
           </button>
         </form>
       </div>
+      <div className='lg:w-1/2 w-full lg:-auto md:h-[500px] h-[350px]'>
+        <Canvas camera={{
+          position: [0,0,5]
+        }}>
+          <directionalLight intensity={2.5}  position={[0,0,1]}/>
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />} >
+            <Fox 
+            position={[0.5, 0.35, 0]}
+            rotation={[12.6,-0.6,0]} 
+            scale={[0.5,0.5,0.5]}/>
+
+          </Suspense>
+
+        </Canvas>
+
+
+      </div>
+
     </section>
   )
 }
