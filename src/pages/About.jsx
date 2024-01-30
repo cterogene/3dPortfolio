c614943434fe74e02a8fa716f82b54cb52bc7f3b
Claudio Terogene <cterogene@gmail.com>
2024-01-29 21:52:55 -0500

    Abot header text added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hi, I'm <span className='blue-gradient_text
+        font-semibold drop-shadow'>Claudio</span>
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p> Bachelor in Computer Science based in Haiti, Software developer, Entrepreneur.
+        </p>
+
+      </div>
+    </section>
   )
 }
 
