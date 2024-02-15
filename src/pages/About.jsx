c99b9197a0beef2c0cd9b92a2e8120e8e409ac14
Claudio Terogene <cterogene@gmail.com>
2024-02-15 09:27:21 -0500

    About page completed and working on project page now

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { skills } from '../constants'
+import { skills, experiences } from '../constants'
+
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
 
 const About = () => {
   return (
@@ -10,7 +13,7 @@ const About = () => {
       </h1>
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
-        <p> Bachelor in Computer Science based in Haiti, Software developer, Entrepreneur.
+        <p> I have a Bachelor in Computer Science, I'm based in Haiti and I work as a Software developer, Entrepreneur.
         </p>
       </div>
 
@@ -44,9 +47,58 @@ const About = () => {
 
         <div className='mt-5 flex flex-col gap-3 text-slate-500'>
           <p> A Software Developer with entry-level experience specializing in user interface design, application development,
-            and continuous improvement. 
-             Adept at identifying opportunities to enhance the user experience and develop new features to improve app functionality.
+            and continuous improvement.
+            Adept at identifying opportunities to enhance the user experience and develop new features to improve app functionality.
           </p>
+        </div>
+
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {experiences.map((experience) => (
+
+              <VerticalTimelineElement 
+                key={experience.company_name}
+                date={experience.date}
+                icon={<div className='flex justify-center items-center w-full h-full'>
+                  <img 
+                    src={experience.icon}
+                    alt={experience.company_name} 
+                    className='w-[60%] h-[60%] object-contain'/>
+                  
+                  </div>}
+                  iconStyle={{background: experience.iconBg}}
+                  contentStyle={{
+                    borderBottom: '8px', borderStyle: 'solid',
+                    borderBottomColor: experience.iconBg,
+                    boxShadow: 'none',
+
+                  }}
+                  >
+                <div>
+                  <h3 className='text-black text-xl font-poppins font-semibold'>
+                    {experience.title}
+                  </h3>
+                  <p className=' text-black-500 font-medium font-base
+                    ' style={{ margin: 0 }}>
+                    {experience.company_name}
+                  </p>
+
+                </div>
+
+                <ul className='my-5 list-disc ml-5 space-y-2'>
+                  {experience.points.map((point, index) => (
+                    <li key={`experience-point-${index}`} className='text-black-500/50 font-normal pl-1 text-sm'>
+                      {point}
+                    </li>
+                  ))}
+
+                </ul>
+
+              </VerticalTimelineElement>
+
+            ))}
+          </VerticalTimeline>
+
         </div>
 
       </div>
